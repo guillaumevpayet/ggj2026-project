71a20902c6c12fbf9944f60f25f1108d26390939
mrgw4 <michaelrgwilson@gmail.com>
2026-01-31 01:06:26 +1300

    Added Main Menu
    Added Menu scene along with basic functionality and assets that can be amended later.

---
+++ Assets/UI Toolkit/MainMenuScript.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    private UIDocument _document;
+
+    private Button _startButton;
+    private Button _exitButton;
+
+    private void Awake()
+    {
+        _document = GetComponent<UIDocument>();
+
+        _startButton = _document.rootVisualElement.Q("PlayButton") as Button;
+        _startButton.RegisterCallback<ClickEvent>(OnPlayGameClick);
+
+        _exitButton = _document.rootVisualElement.Q("ExitButton") as Button;
+        _exitButton.RegisterCallback<ClickEvent>(OnExitButtonClick);
+    }
+
+    private void OnEnable()
+    {
+        _document = GetComponent<UIDocument>();
+
+        _startButton = _document.rootVisualElement.Q("StartButton") as Button;
+        _startButton.RegisterCallback<ClickEvent>(OnPlayGameClick);
+
+        _exitButton = _document.rootVisualElement.Q("ExitButton") as Button;
+        _exitButton.RegisterCallback<ClickEvent>(OnExitButtonClick);
+    }
+
+
+    private void OnDisable()
+    {
+        _startButton.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        _exitButton.UnregisterCallback<ClickEvent>(OnExitButtonClick);
+    }
+
+    private void OnExitButtonClick(ClickEvent evt)
+    {
+        Debug.Log("EXIT BUTTON PRESSED!");
+        Application.Quit();
+    }
+
+    private void OnPlayGameClick(ClickEvent evt)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
